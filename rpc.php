e5663290d5052bc29d89f36d3b04188e8bfd8f20
Philipp Hotz <p.hotz@posteo.net>
2018-02-02 11:05:15 +0100

    Update rpc definitions

--- rpc.php
+++ rpc.php
@@ -1,120 +1,148 @@
 <?php
 
 
-kirby()->set('rpc', 'rpc_unassign_page', function ($pageUuid, $username) {
-  $page = get_page_by_uuid($pageUuid);
+kirby()->set('rpc', [
+  'method' => 'rpc_unassign_page',
+  'roles' => ['admin'],
+  'action' => function ($pageUuid, $username) {
+    $page = get_page_by_uuid($pageUuid);
 
-  $user = kirby()->site()->users()->find($username);
+    $user = kirby()->site()->users()->find($username);
 
-  unassign_page($user, $page);
+    unassign_page($user, $page);
 
-  return [];
-});
-
-kirby()->set('rpc', 'rpc_assign_page_to_user', function ($pageUuid, $username) {
-  $user = kirby()->site()->users()->find($username);
-
-  $page = get_page_by_uuid($pageUuid);
-
-  assign_page_to_user($user, $page);
-
-  return [];
-});
-
-kirby()->set('rpc', 'rpc_mark_as_completed', function ($pageUuid) {
-  $user = kirby()->site()->user();
-
-  $page = get_page_by_uuid(intval($pageUuid));
-
-  user_read_page($user, $page);
-
-  return [];
-});
-
-kirby()->set('rpc', 'rpc_get_users', function ($pageUuid) {
-  $users = kirby()->site()->users();
-
-  $res = [];
-
-  $page = get_page_by_uuid($pageUuid);
-
-  foreach ($users as $user) {
-
-    $is_assigned = page_assigned_to_user($user, $page);
-
-    $done = $is_assigned && user_has_read_all_infobits($user, $page);
-
-    if (!$is_assigned) {
-      $status = 'unassigned';
-    }
-    else if ($done) {
-      $status = 'done';
-    }
-    else {
-      $status = 'incomplete';
-    }
-
-    $res[] = [
-      'name' => $user->username(),
-      'status' => $status
-    ];
-
+    return [];
   }
+]);
 
-  return $res;
-});
+kirby()->set('rpc', [
+  'method' => 'rpc_assign_page_to_user',
+  'roles' => ['admin'],
+  'action' => function ($pageUuid, $username) {
+    $user = kirby()->site()->users()->find($username);
 
-kirby()->set('rpc', 'user_field_add_page', function ($username, $field, $pageUuid) {
-  $user = kirby()->site()->users()->find($username);
+    $page = get_page_by_uuid($pageUuid);
 
-  $page = get_page_by_uuid($pageUuid);
+    assign_page_to_user($user, $page);
 
-  Pagelist\add($user, $field, $page);
-});
+    return [];
+  }
+]);
 
-kirby()->set('rpc', 'user_field_get_pages', function ($username, $field) {
-  $topics = kirby()
-          ->site()
-          ->index()
-          ->filterBy('template', 'topic');
+kirby()->set('rpc', [
+  'method' => 'rpc_mark_as_completed',
+  'action' => function ($pageUuid) {
+    $user = kirby()->site()->user();
 
-  $r = [];
+    $page = get_page_by_uuid(intval($pageUuid));
 
-  $user = kirby()->site()->users()->find($username);
+    user_read_page($user, $page);
 
-  foreach ($topics as $topic) {
+    return [];
+  }
+]);
 
-    $completedIds = Pagelist\ids($user, INFOBITS_FIELD);
+kirby()->set('rpc', [
+  'method' => 'rpc_get_users',
+  'roles' => ['admin'],
+  'action' => function ($pageUuid) {
+    $users = kirby()->site()->users();
 
-    $infobits = $topic->children();
+    $res = [];
 
-    $completed = 0;    
+    $page = get_page_by_uuid($pageUuid);
 
-    foreach ($infobits as $infobit) {
-      $uuid = $infobit->uuid()->value;
+    foreach ($users as $user) {
 
-      if (in_array($uuid, $completedIds)) {
-        $completed++;
+      $is_assigned = page_assigned_to_user($user, $page);
+
+      $done = $is_assigned && user_has_read_all_infobits($user, $page);
+
+      if (!$is_assigned) {
+        $status = 'unassigned';
       }
+      else if ($done) {
+        $status = 'done';
+      }
+      else {
+        $status = 'incomplete';
+      }
+
+      $res[] = [
+        'name' => $user->username(),
+        'status' => $status
+      ];
+
     }
 
-    $r[] = [
-      'title' => $topic->title()->value,
-      'uuid' => $topic->uuid()->value,
-      'active' => Pagelist\has($user, $field, $topic),
-      'infobits' => $infobits->count(),
-      'completed' => $completed
-    ];
-
+    return $res;
   }
+]);
 
-  return $r;
-});
+kirby()->set('rpc', [
+  'method' => 'user_field_add_page',
+  'roles' => ['admin'],
+  'action' => function ($username, $field, $pageUuid) {
+    $user = kirby()->site()->users()->find($username);
 
-kirby()->set('rpc', 'user_field_remove_page', function ($username, $field, $pageUuid) {
-  $user = kirby()->site()->users()->find($username);
+    $page = get_page_by_uuid($pageUuid);
 
-  $page = get_page_by_uuid($pageUuid);
+    Pagelist\add($user, $field, $page);
+  }
+]);
 
-  Pagelist\remove($user, $field, $page);  
-});
+kirby()->set('rpc', [
+  'method' => 'user_field_get_pages',
+  'roles' => ['admin'],
+  'action' => function ($username, $field) {
+    $topics = kirby()
+            ->site()
+            ->index()
+            ->filterBy('template', 'topic');
+
+    $r = [];
+
+    $user = kirby()->site()->users()->find($username);
+
+    foreach ($topics as $topic) {
+
+      $completedIds = Pagelist\ids($user, INFOBITS_FIELD);
+
+      $infobits = $topic->children();
+
+      $completed = 0;    
+
+      foreach ($infobits as $infobit) {
+        $uuid = $infobit->uuid()->value;
+
+        if (in_array($uuid, $completedIds)) {
+          $completed++;
+        }
+      }
+
+      $r[] = [
+        'title' => $topic->title()->value,
+        'uuid' => $topic->uuid()->value,
+        'active' => Pagelist\has($user, $field, $topic),
+        'infobits' => $infobits->count(),
+        'completed' => $completed
+      ];
+
+    }
+
+    return $r;
+  }
+]);
+             
+
+kirby()->set('rpc', [
+  'method' => 'user_field_remove_page',
+  'roles' => ['admin'],
+  'action' => function ($username, $field, $pageUuid) {
+    $user = kirby()->site()->users()->find($username);
+
+    $page = get_page_by_uuid($pageUuid);
+
+    Pagelist\remove($user, $field, $page);  
+  }
+]);
