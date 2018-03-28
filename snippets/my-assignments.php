6823fdc2ba16251b484c518f6355ab6873c72c36
Philipp Hotz <p.hotz@posteo.net>
2018-03-28 12:11:00 +0200

    Display a message when a user has now assignments

--- snippets/my-assignments.php
+++ snippets/my-assignments.php
@@ -1,21 +1,37 @@
+
+<?php
+$unreadInfobits = get_unread_infobits(kirby()->site()->user());
+?>
+
 <div id="my-assignments" class="ui modal">
   <div class="header">Your Assignments</div>
   <div class="content">
 
+    <?php if (0 == count($unreadInfobits)): ?>
+
+    <div class="ui icon message">
+      <i class="thumbs up icon"></i>
+      <div class="content">
+        <div class="header">
+          You currently have no assignments.
+        </div>
+        <p>
+          When your team head assigns new assignments to you, they will show up here.
+        </p>
+      </div>
+    </div>
+
+    <?php else: ?>
     <div class="ui relaxed divided list">
-      <?php foreach (topics_assigned_to_user(kirby()->site()->user()) as $topic): ?>
 
-      <?php foreach (get_infobits($topic) as $infobit): ?>
-
-      <?php if (!has_read_infobit(kirby()->site()->user(), $infobit)): ?>
-
+      <?php foreach ($unreadInfobits as $infobit): ?>
 
       <div class="item">
         <i class="large github middle aligned icon"></i>
         <div class="content">
           <span class="header">
-            <a href="<?php echo $topic->url() ?>">
-              <?php echo $topic->title()->value ?>
+            <a href="<?php echo $infobit->parent()->url() ?>">
+              <?php echo $infobit->parent()->title()->value ?>
             </a>
             <span style="color: rgba(0,0,0,.7);">/</span>
             <a href="<?php echo $infobit->url() ?>">
@@ -23,17 +39,14 @@
             </a>
           </span>
           <div class="description">
-            <?php echo infobit_gist($infobit); ?>
+            <!--<?php echo infobit_gist($infobit); ?>-->
           </div>
         </div>
       </div>
 
+      <?php endforeach; ?>
 
       <?php endif; ?>
-
-      <?php endforeach; ?>
-
-      <?php endforeach; ?>
 
     </div>
   </div>
