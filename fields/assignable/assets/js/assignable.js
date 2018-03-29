a899e4831b5e6f8f333877cf4aebeda3bcbeeec4
Philipp Hotz <p.hotz@posteo.net>
2018-03-29 11:41:32 +0200

    Clean up

--- fields/assignable/assets/js/assignable.js
+++ fields/assignable/assets/js/assignable.js
@@ -1,5 +1,27 @@
 (function () {
 
+  function rpc(method, params) {
+    return new Promise((resolve, reject) => {
+      let r = new XMLHttpRequest();
+      r.open('POST', API_ENDP, true);
+      r.responseType = 'json';
+      r.onreadystatechange = () => {
+        if (r.readyState != 4) return;
+
+        if (r.status == 200) resolve(r.response.result);
+        else reject();
+      };
+      let body = {
+        'jsonrpc': '2.0',
+        'id': 1,
+        'method': method,
+        'params': params
+      };
+      r.send(JSON.stringify(body));
+    });
+    
+  }
+
   const API_ENDP = '/jsonrpc';
 
   class Assigner {
@@ -9,24 +31,43 @@
       this.handleClickBound_ = this.handleClick_.bind(this);
       this.el_ = el;
       this.pageUuid_ = pageUuid;
-      this.users_ = [];
 
       this.el_.addEventListener('click', this.handleClickBound_);
       this.reload_();
       this.el_.classList.add('assignable-field');
     }
 
+    clear_() {
+      let r = document.createRange();
+      r.selectNodeContents(this.el_);
+      r.deleteContents();
+    }
+
     reload_() {
       Assigner
         .loadUsers(this.pageUuid_)
         .then((users) => {
-          this.users_ = users;
-          this.renderUsers_(users)
-        });
+
+          const f = document.createDocumentFragment();
+
+          users.forEach((u) => {
+            let el = Assigner.renderUser_(u['name'], u['status']);
+            f.appendChild(el);
+          });
+
+          this.clear_();
+          this.el_.appendChild(f);
+        })
+        .catch((e) => this.displayError_('Unable to load users'));
+    }
+
+    displayError_(msg) {
+      this.clear_();
+      this.el_.innerHTML = `An error occurred: ${msg}. Please make sure you are logged in.`;
     }
 
     handleClick_(ev) {
-      if (null === ev.target.dataset.user) return;
+      if (undefined === ev.target.dataset.user) return;
 
       let user = ev.target.dataset.user;
       let status = ev.target.dataset.status;
@@ -34,12 +75,14 @@
       if ('unassigned' === status) {
         Assigner
           .assignUser(user, this.pageUuid_)
-          .then(this.reloadBound_);
+          .then(this.reloadBound_)
+          .catch(() => this.displayError_('Unable to remove the assignment'));
       }
       else {
         Assigner
           .unassignUser(user, this.pageUuid_)
-          .then(this.reloadBound_);
+          .then(this.reloadBound_)
+          .catch(() => this.displayError_('Unable to assign the topic'));
       }
     }
 
@@ -59,82 +102,7 @@
       }
     }
 
-    renderUsers_(users) {
-      this.el_.innerHTML = '';
-
-      users.forEach((user) => {
-
-        let el = Assigner.renderUser_(1, user['name'], user['status']);
-
-
-
-        this.el_.appendChild(el);
-      });
-    }
-
-    static loadUsers(pageUuid) {
-      return new Promise((resolve, reject) => {
-        let r = new XMLHttpRequest();
-        r.open('POST', API_ENDP, true);
-        r.responseType = 'json';
-        r.onreadystatechange = () => {
-          if (r.readyState != 4 || r.status != 200) return;
-
-          resolve(r.response['result']);
-        };
-
-        let body = {
-          'jsonrpc': '2.0',
-          'method': 'rpc_get_users',
-          'params': [pageUuid],
-          'id': 1
-        };
-
-        r.send(JSON.stringify(body));
-      });
-    }
-
-    static assignUser(username, pageUuid) {
-      return new Promise((resolve, reject) => {
-
-        let r = new XMLHttpRequest();
-        r.open('POST', API_ENDP, true);
-        r.responseType = 'json';
-        r.onreadystatechange = () => {
-          if (r.readyState != 4 || r.status != 200) return;
-
-          resolve();
-        };
-        let body = {
-          'jsonrpc': '2.0',
-          'id': 1,
-          'method': 'rpc_assign_page_to_user',
-          'params': [pageUuid, username]
-        };
-        r.send(JSON.stringify(body));
-      });      
-    }
-
-    static unassignUser(username, pageUuid) {
-      return new Promise((resolve, reject) => {
-        let r = new XMLHttpRequest();
-        r.open('POST', API_ENDP, true);
-        r.responseType = 'json';
-        r.onreadystatechange = () => {
-          if (r.readyState != 4 || r.status != 200) return;
-          resolve();
-        };
-        let body = {
-          'jsonrpc': '2.0',
-          'id': 1,
-          'method': 'rpc_unassign_page',
-          'params': [pageUuid, username]
-        };
-        r.send(JSON.stringify(body));
-      });
-    }
-
-    static renderUser_(id, name, status) {
+    static renderUser_(name, status) {
       let el = document.createElement('div');
 
       el.classList.add('assignable-field__user');
@@ -151,6 +119,21 @@
       el.innerHTML = `${name}`;
       el.title = Assigner.statusStr(status);
       return el;
+    }
+
+
+    // wrappers for rpc methods
+
+    static loadUsers(pageUuid) {
+      return rpc('rpc_get_users', [pageUuid]);
+    }
+
+    static assignUser(username, pageUuid) {
+      return rpc('rpc_assign_page_to_user', [pageUuid, username]);
+    }
+
+    static unassignUser(username, pageUuid) {
+      return rpc('rpc_unassign_page', [pageUuid, username]);
     }
   }
 
